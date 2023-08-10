572dd5cb3919bb5fe7500e853cf39f8497048d5c
Oleksiy Vekhulevskyy <alexvekh@yahoo.com>
2023-08-10 12:22:43 -0400

    homework is done

--- 9.js
+++ 9.js
@@ -1,12 +1,24 @@
 console.log("Завдання: 9 ==============================");
 
 function task9() {
+  function fetchWithError() {
+    const errorMessage = "Помилка при з'єднанні з сервером";
+    //return Promise.reject(errorMessage);  // error.message blocks the error.stack
+    return Promise.reject(new Error(errorMessage));
+  }
   // Створюємо функцію fetchWithError, яка симулює помилку при запиті до сервера.
   // Створюємо константу errorMessage в яку записуємо рядок "Помилка при з'єднанні з сервером"
   // Використовуємо Promise.reject(errorMessage) для створення вже відхиленого промісу.
   // Викликаємо функцію fetchWithError
   // Якщо проміс виконаний успішно виводимо в консоль дані які він повертає
   // Якщо проміс виконаний з помилкою виводимо в консоль помилку
+  fetchWithError()
+    .then((data) => console.log(data))
+    .catch((error) => {
+      //console.log(error.massage);
+      //console.log(error.stack);
+      console.log(error);
+    });
 }
 
 // Викликаємо функцію task10
---
+++ index.js
@@ -0,0 +1,2 @@
+import { task1 } from "./1.js";
+task1();
